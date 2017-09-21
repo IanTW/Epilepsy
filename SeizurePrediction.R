c62dc466301f824bf02452ce120ad04b254e34f6
Your Name <you@example.com>
2017-09-21 23:20:04 +0100

    Edit

--- SeizurePrediction.R
+++ SeizurePrediction.R
@@ -13,45 +13,63 @@
 # R code for implementing a machine learning classification experiment to compare the        #
 # performance of SVM and neural network classifiers used for epileptic seizure prediction    #
 #                                                                                            #
-##############################################################################################
+############################# SET UP AND LOAD PACKAGES #######################################
 
-#Install R packages
-#List of required packages
+# Install R packages if required
+# List of required packages
 list.of.packages <- c("R.matlab",
-                      "dplyr")
+                      "dplyr",
+                      "eegkit")
 
-#Create list of required new packages
+# Create list of required new packages
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-#If non-empty list is returned, install packages
+# If non-empty list is returned, install packages
 if(length(new.packages)) install.packages(new.packages)
 
-#Load libraries
+# Load packages
 lapply(list.of.packages,function(x){library(x,character.only=TRUE)})
 
-#Set working directory
+############################### LINK TO OTHER SOURCE FILES ###################################
+# Links to other R files
+# source("otherRfile.R")
+# etc
+
+############################# CHECK INPUT FILES FOR ERRORS ###################################
+
+# Set working directory
 setwd('/home/ianw/Epilepsy')
 
-#Make list of all data files
-#Creates a dataframe with all file details - see ?file.info
+# Make list of all data files
+# Creates a dataframe with all file details - see ?file.info
 listfiles <- file.info(list.files(path = "./Data", pattern = NULL, all.files = FALSE,
                                   full.names = FALSE, recursive = FALSE,
                                   ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE))
 
-#Extract rowname to get file names
+# Create list using rownames
 listfiles <- as.list(rownames(listfiles))
 
-#Set working directory for data directory
+# Set working directory for data directory
 setwd('./Data')
 
-#Loop for all files
-
+# Loop to check all files for errors
+# Reads in files and gives error if encountered
 for (filename in listfiles){
-
   tryCatch({
     a <- readMat(filename)
   }, error=function(e){cat("ERROR :",conditionMessage(e), filename, "\n")})
-  
 }
+
+############################### PREPROCESSING OF DATA FILES ##################################
+
+
+
+
+# Open file
+
+
+
+
+
 
 # from discussion on Kaggle....
 pat <- readMat('\\Data\Dog_1_interictal_segment_0018.mat')
