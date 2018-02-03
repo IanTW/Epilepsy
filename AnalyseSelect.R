cb1943a031910a8b63f3ea63d8da057cd0fde066
Ian Watson <ian.watson@imaginegroup.ie>
2018-02-03 14:44:12 +0000

    Edit

--- AnalyseSelect.R
+++ AnalyseSelect.R
@@ -1,85 +1,10 @@
-setwd("~/Epilepsy/Select/FFT")
+
 library(data.table)
 library(plotly)
 library(stringr)
 library(plyr)
 
-# All selections
-
-load("60-50_LVQ.rda")
-a <- list(cols)
-load("60-50_RFE.rda")
-b <- list(cols)
-load("60-00_LVQ.rda")
-c <- list(cols)
-load("60-00_RFE.rda")
-d <- list(cols)
-load("30-50_LVQ.rda")
-e <- list(cols)
-load("30-50_RFE.rda")
-f <- list(cols)
-load("30-00_LVQ.rda")
-g <- list(cols)
-load("30-00_RFE.rda")
-h <- list(cols)
-
-merged <- list(a,b,c,d,e,f,g,h)
-
-x <- table(unlist(merged))
-x <-as.data.frame(x)
-n <- nrow(x)-2
-x <- x[1:n,]
-x$Var1 <- as.character(x$Var1)
-# For Stat
-#y <- read.table(text = x$Var1, sep = "_", colClasses = "character")
-# For FFT
-y <- str_split_fixed(as.character(x$Var1),'_', 3)
-
-z <- cbind(x,y)
-z <- as.data.table(z)
-colnames(z) <- c("Feature", "Freq", "Lab", "Chan", "Feats")
-dat <- z[,.(Sum = sum(Freq)), by = .(Feats)]
-
-#Rename factors
-dat$Feats <- revalue(dat$Feats, c("Alpha_Rel" = "Alpha_Norm",
-                     "GammaHigh" = "Gamma_High",
-                     "GammaLow" = "Gamma_Low",
-                     "Beta_Rel" = "Beta_Norm",
-                     "Delta_Rel" = "Delta_Norm",
-                     "GammaHigh_Rel" = "Gamma_High_Norm",
-                     "GammaLow_Rel" = "Gamma_Low_Norm",
-                     "Theta_Rel" = "Theta_Norm",
-                     "Total_Power" = "Total_Energy"))
-
-# Set up margins
-m <- list(
-  l = 130,
-  r = 30,
-  b = 60,
-  t = 20,
-  pad = 4)
-
-yax <- list(
-  title = "")
-
-xax <- list(
-  title = "Number of Times Selected")
-
-Feats <- dat$Feats
-Counts <- dat$Sum
-dat <- data.frame(Feats, Counts, stringsAsFactors = FALSE)
-dat$Feats <- factor(dat$Feats, levels = unique(dat$Feats)[order(dat$Counts, decreasing = FALSE)])
-plot_ly(dat, x = ~Counts, y = ~Feats, type = "bar", orientation = 'h') %>%
-  layout(margin = m, yaxis = yax, xaxis = xax)
-
-##########################################
-
-setwd("~/Epilepsy/Select/Stat")
-library(data.table)
-library(plotly)
-library(stringr)
-library(plyr)
-
+#Stat
 # All selections
 
 load("60-50_LVQ.rda")
@@ -116,36 +41,99 @@ z <- as.data.table(z)
 z$V1 <- NULL
 z$V2 <- NULL
 colnames(z) <- c("Feature", "Freq", "Feats")
-dat <- z[,.(Sum = sum(Freq)), by = .(Feats)]
+dat1 <- z[,.(Sum = sum(Freq)), by = .(Feats)]
 
 # Set up margins
 m <- list(
   l = 70,
   r = 10,
+  b = 140,
+  t = 20,
+  pad = 4)
+
+yax <- list(
+  title = "", tickfont = list(size =11))
+xax <- list(
+  title = "Number of Times Selected")
+
+Feats <- dat1$Feats
+Counts <- dat1$Sum
+dat1 <- data.frame(Feats, Counts, stringsAsFactors = FALSE)
+dat1$Feats <- factor(dat1$Feats, levels = unique(dat1$Feats)[order(dat1$Counts, decreasing = FALSE)])
+p1 <- plot_ly(dat1, x = ~Counts, y = ~Feats, type = "bar", orientation = 'h', name= "Statistics") %>%
+  layout(margin = m, yaxis = yax, xaxis = xax)
+
+######################################################
+# FFT
+# All selections
+
+load("60-50_LVQ.rda")
+a <- list(cols)
+load("60-50_RFE.rda")
+b <- list(cols)
+load("60-00_LVQ.rda")
+c <- list(cols)
+load("60-00_RFE.rda")
+d <- list(cols)
+load("30-50_LVQ.rda")
+e <- list(cols)
+load("30-50_RFE.rda")
+f <- list(cols)
+load("30-00_LVQ.rda")
+g <- list(cols)
+load("30-00_RFE.rda")
+h <- list(cols)
+
+merged <- list(a,b,c,d,e,f,g,h)
+
+x <- table(unlist(merged))
+x <-as.data.frame(x)
+n <- nrow(x)-2
+x <- x[1:n,]
+x$Var1 <- as.character(x$Var1)
+# For Stat
+#y <- read.table(text = x$Var1, sep = "_", colClasses = "character")
+# For FFT
+y <- str_split_fixed(as.character(x$Var1),'_', 3)
+
+z <- cbind(x,y)
+z <- as.data.table(z)
+colnames(z) <- c("Feature", "Freq", "Lab", "Chan", "Feats")
+dat2 <- z[,.(Sum = sum(Freq)), by = .(Feats)]
+
+#Rename factors
+dat2$Feats <- revalue(dat2$Feats, c("Alpha_Rel" = "Alpha_Norm",
+                     "GammaHigh" = "Gamma_High",
+                     "GammaLow" = "Gamma_Low",
+                     "Beta_Rel" = "Beta_Norm",
+                     "Delta_Rel" = "Delta_Norm",
+                     "GammaHigh_Rel" = "Gamma_High_Norm",
+                     "GammaLow_Rel" = "Gamma_Low_Norm",
+                     "Theta_Rel" = "Theta_Norm",
+                     "Total_Power" = "Total_Energy"))
+
+# Set up margins
+m <- list(
+  l = 130,
+  r = 30,
   b = 60,
   t = 20,
   pad = 4)
 
 yax <- list(
-  title = "")
+  title = "", tickfont = list(size =11))
 
 xax <- list(
   title = "Number of Times Selected")
 
-Feats <- dat$Feats
-Counts <- dat$Sum
-dat <- data.frame(Feats, Counts, stringsAsFactors = FALSE)
-dat$Feats <- factor(dat$Feats, levels = unique(dat$Feats)[order(dat$Counts, decreasing = FALSE)])
-plot_ly(dat, x = ~Counts, y = ~Feats, type = "bar", orientation = 'h') %>%
+Feats <- dat2$Feats
+Counts <- dat2$Sum
+dat2 <- data.frame(Feats, Counts, stringsAsFactors = FALSE)
+dat2$Feats <- factor(dat2$Feats, levels = unique(dat2$Feats)[order(dat2$Counts, decreasing = FALSE)])
+p2 <- plot_ly(dat2, x = ~Counts, y = ~Feats, type = "bar", orientation = 'h', name = "Spectral") %>%
   layout(margin = m, yaxis = yax, xaxis = xax)
 
 ##########################################
-
-setwd("~/Epilepsy/Select/Both")
-library(data.table)
-library(plotly)
-library(stringr)
-library(plyr)
 
 # All selections
 
@@ -179,7 +167,7 @@ y <- str_split_fixed(as.character(x$Var1),'_', 3)
 z <- cbind(x,y)
 z <- as.data.table(z)
 colnames(z) <- c("Feature", "Freq", "Lab", "Chan", "Feats")
-dat <- z[,.(Sum = sum(Freq)), by = .(Feats)]
+dat3 <- z[,.(Sum = sum(Freq)), by = .(Feats)]
 
 # Set up margins
 m <- list(
@@ -190,17 +178,20 @@ m <- list(
   pad = 4)
 
 yax <- list(
-  title = "")
+  title = "", tickfont = list(size =11))
 
 xax <- list(
   title = "Number of Times Selected")
 
-Feats <- dat$Feats
-Counts <- dat$Sum
-dat <- data.frame(Feats, Counts, stringsAsFactors = FALSE)
-dat$Feats <- factor(dat$Feats, levels = unique(dat$Feats)[order(dat$Counts, decreasing = FALSE)])
-plot_ly(dat, x = ~Counts, y = ~Feats, type = "bar", orientation = 'h') %>%
+Feats <- dat3$Feats
+Counts <- dat3$Sum
+dat3 <- data.frame(Feats, Counts, stringsAsFactors = FALSE)
+dat3$Feats <- factor(dat3$Feats, levels = unique(dat3$Feats)[order(dat3$Counts, decreasing = FALSE)])
+p3 <- plot_ly(dat3, x = ~Counts, y = ~Feats, type = "bar", orientation = 'h', name = "Combined") %>%
   layout(margin = m, yaxis = yax, xaxis = xax)
+
+subplot(p1,p2,p3, nrows = 3, shareX = TRUE, heights = c(0.2, 0.4, 0.4)) %>%  layout(legend = list(x=0.6,y=0.1))
+
 
 #####################################################################################
 
@@ -573,6 +564,8 @@ subplot(p1, p2, p3, p4, nrows = 4) %>%
   layout(margin = m, legend = list(x = 0.7, y = 0.6))
 
 ################################################################################
+
+
 
 
 
